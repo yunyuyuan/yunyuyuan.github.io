9cd69cabc20519e15e0223441e53276b3863a390
chenqiang <1607439239@qq.com>
2020-09-08 15:36:38 +0800

    init vue

--- another/blog/js/code.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.copy-all').click(function () {
-        $('.copy-src')[0].select();
-        document.execCommand('copy');
-        alert('复制成功');
-    })
-});
