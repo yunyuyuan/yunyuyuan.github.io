49a907bdae0a8f9eb4857aa35f96aa940dc01aac
yunyuyuan <326178275@qq.com>
2020-09-14 11:08:31 +0800

    article-detail

--- src/views/article/Detail.vue
+++ src/views/article/Detail.vue
@@ -1,13 +1,17 @@
 <template>
-    <div></div>
+  <div class="detail">
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Detail"
-    }
+export default {
+  name: "Detail"
+}
 </script>
 
 <style scoped lang="scss">
+.detail {
 
+}
 </style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,13 +1,45 @@
 <template>
+  <div class="article">
+    <img :src="articleImage">
+    <div class="mask"></div>
     <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+import articleImage from '@/image/article.jpg'
+
+export default {
+  name: "index",
+  data() {
+    return {
+      articleImage,
     }
+  }
+}
 </script>
 
 <style scoped lang="scss">
+.article {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  flex-shrink: 0;
 
+  > .mask, > img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  > img {
+    object-fit: cover;
+  }
+
+  > .mask {
+    background: rgba(0, 0, 0, 0.15);
+  }
+}
 </style>
